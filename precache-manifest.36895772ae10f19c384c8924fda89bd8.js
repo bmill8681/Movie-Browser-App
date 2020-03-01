537ccd73bedaf02e7def2f433db24813f3ce5ccd
Brett Miller <38405413+LLimb@users.noreply.github.com>
2020-03-01 12:25:52 -0700

    Updates

---
+++ precache-manifest.36895772ae10f19c384c8924fda89bd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564f23eff50c76cc1dcfbc9e6f470cac",
+    "url": "/Movie-Browser-App/index.html"
+  },
+  {
+    "revision": "2b66af136c9960cb65b4",
+    "url": "/Movie-Browser-App/static/css/main.b9da6b7a.chunk.css"
+  },
+  {
+    "revision": "88ba258a9042bf570555",
+    "url": "/Movie-Browser-App/static/js/2.b7bbf259.chunk.js"
+  },
+  {
+    "revision": "395404804f0eba41b11435acf8e831a6",
+    "url": "/Movie-Browser-App/static/js/2.b7bbf259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b66af136c9960cb65b4",
+    "url": "/Movie-Browser-App/static/js/main.a6835461.chunk.js"
+  },
+  {
+    "revision": "3467689fe98876f78776",
+    "url": "/Movie-Browser-App/static/js/runtime-main.954657bf.js"
+  },
+  {
+    "revision": "07d226ce19b763c391296e471a82230b",
+    "url": "/Movie-Browser-App/static/media/bg.07d226ce.png"
+  }
+]);
